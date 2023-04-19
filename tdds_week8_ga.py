c884bec4523d383f30a8800b0ee32e8121df2784
Nooha Rahman <89066195+increasemeinknowledge@users.noreply.github.com>
2023-04-19 16:37:09 +0530

    Update tdds_week8_ga.py

--- tdds_week8_ga.py
+++ tdds_week8_ga.py
@@ -12,9 +12,9 @@ To find the largest among the 3 given numbers
 st.header('Enter 3 numbers')
 
 def user_input_features():
-    num1 = st.number_input("num1",min_value=-200000,max_value=200000,step=1)
-    num2 = st.number_input("num2",min_value=-200000,max_value=200000,step=1)
-    num3 = st.number_input("num3",min_value=-200000,max_value=200000,step=1)
+    num1 = st.number_input("num1")
+    num2 = st.number_input("num2")
+    num3 = st.number_input("num3")
     nums=[num1,num2,num3]
     return (nums)
 
