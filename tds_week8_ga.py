92b411cf9d4ba210398e2eef7d00e3011d3ec5e9
Nooha Rahman <89066195+increasemeinknowledge@users.noreply.github.com>
2023-04-19 17:39:24 +0530

    Update tds_week8_ga.py

--- tds_week8_ga.py
+++ tds_week8_ga.py
@@ -12,9 +12,9 @@ st.title("Find the Largest Number")
 st.write("Enter three numbers below to find the largest among them.")
 
 # Create input fields for three numbers
-a = st.number_input("Enter the first number:",value=0)
-b = st.number_input("Enter the second number:",value=0)
-c = st.number_input("Enter the third number:",value=0)
+a = st.number_input("Enter the first number:")
+b = st.number_input("Enter the second number:")
+c = st.number_input("Enter the third number:")
 
 # Create a button to trigger the find_largest function
 if st.button("Find"):
@@ -22,4 +22,4 @@ if st.button("Find"):
     largest = find_largest(a, b, c)
 
     # Display the result
-    st.write(f"The largest number is {largest} .")
+    st.write(f"{largest} is the largest number.")
