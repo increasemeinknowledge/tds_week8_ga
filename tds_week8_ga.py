de2cd34f7d7e74067219963228d9a4a2f482bcf1
Nooha Rahman <89066195+increasemeinknowledge@users.noreply.github.com>
2023-04-19 16:23:33 +0530

    Update tds_week8_ga.py

--- tds_week8_ga.py
+++ tds_week8_ga.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 st.write("""
-# Largest of 3
+# Largest of 3 numbers
 This app tells the largest number out of three
 """)
 #Get Input
@@ -10,14 +10,21 @@ This app tells the largest number out of three
 st.header('Enter 3 numbers')
 
 def user_input_features():
-    num1 = st.number_input("num1",min_value=-200000,max_value=200000,step=1)
-    num2 = st.number_input("num2",min_value=-200000,max_value=200000,step=1)
-    num3 = st.number_input("num3",min_value=-200000,max_value=200000,step=1)
-    nums=[num1,num2,num3]
-    return (nums)
+   num1 = float(input())
+   num2 = float(input())
+   num3 = float(input())
+
+if (num1 >= num2) and (num1 >= num3):
+   largest = num1
+elif (num2 >= num1) and (num2 >= num3):
+   largest = num2
+else:
+   largest = num3
+
+
 
 nums = user_input_features()
-output=max(nums)
+output=largest
 
 st.subheader('Maximum amongst the 3 is:')
 st.write(output)
